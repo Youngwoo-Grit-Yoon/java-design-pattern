29b0144a9cec93c200e3b511732e742c96a79944
윤영우 <macair1995@naver.com>
2023-12-27 00:49:01 +0900

    Add command class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,22 @@
 package com.example.demo;
 
+import com.example.demo.design.patterns.AbstractFactory.AbstractFactory;
+import com.example.demo.design.patterns.ChainOfResponsibility.ChainOfResponsibility;
+import com.example.demo.design.patterns.Command.Command;
+import com.example.demo.design.patterns.DesignPattern;
+import com.example.demo.design.patterns.FactoryMethod.FactoryMethod;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
+	// Design Patterns
+	DesignPattern abstractFactory = new AbstractFactory();
+	DesignPattern chainOfResponsibility = new ChainOfResponsibility();
+	DesignPattern factoryMethod = new FactoryMethod();
+	DesignPattern command = new Command();
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
---
+++ src/main/java/com/example/demo/design/patterns/Command/Command.java
@@ -0,0 +1,10 @@
+package com.example.demo.design.patterns.Command;
+
+import com.example.demo.design.patterns.DesignPattern;
+
+public class Command implements DesignPattern {
+    @Override
+    public void run() {
+
+    }
+}
